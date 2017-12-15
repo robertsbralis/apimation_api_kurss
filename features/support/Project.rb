4cfddbaf3cb0339acbdb5e005ecc988431ce89f5
Roberts Bralis <roberts.bralis@testdevlab.com>
2017-12-15 14:21:31 +0200

    Added the first homework case: add project+add 2 environments and 2 global vars and delete them

---
+++ features/support/Project.rb
@@ -0,0 +1,26 @@
+class Project
+  attr_accessor :name
+  attr_accessor :environments
+  attr_accessor :global_variables
+  attr_accessor :project_id
+  attr_accessor :type
+
+  def initialize(name)
+    @name=name
+    @type='basic'
+    @environments=[]
+    @global_variables ||= []
+  end
+
+  def set_environments(env,id)
+    hash={'name'=>env,'id'=>id}
+    @environments= @environments.push(hash)
+  end
+
+  def set_project_id(id)
+    @project_id=id
+  end
+  def set_global_vars
+
+  end
+end
--- features/support/env.rb
+++ features/support/env.rb
@@ -1,6 +1,7 @@
 Before() do
   puts "Before hook.This will work before every test case!"
   @test_user = User.new('bralisroberts@gmail.com','parole')
+  @project_array=[]
 end
 After() do
   puts 'This happens after a test'
